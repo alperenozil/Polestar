15b32884832f7e43b7a110206e954b1bed5da6a7
Necdet Alperen Özil <b21427234@cs.hacettepe.edu.tr>
2022-12-12 12:54:17 +0100

    Create solution class

--- main/java/alperenozil/polestar/test/Main.java
+++ main/java/alperenozil/polestar/test/Main.java
@@ -2,8 +2,7 @@ package alperenozil.polestar.test;
 
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("alperen");
-
+        Solution solution=new Solution();
+        System.out.println(solution.getName());
     }
 }
---
+++ main/java/alperenozil/polestar/test/Solution.java
@@ -0,0 +1,9 @@
+package alperenozil.polestar.test;
+
+public class Solution {
+    private String name="al";
+
+    public String getName() {
+        return name;
+    }
+}
