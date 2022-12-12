767c33c5fc55f335ea9bebc13a210b3baa348cf4
Necdet Alperen Özil <b21427234@cs.hacettepe.edu.tr>
2022-12-12 13:12:14 +0100

    Add findFirstIllegalCharacter method

--- main/java/alperenozil/polestar/test/Main.java
+++ main/java/alperenozil/polestar/test/Main.java
@@ -3,6 +3,6 @@ package alperenozil.polestar.test;
 public class Main {
     public static void main(String[] args) {
         Solution solution=new Solution();
-        System.out.println(solution.getName());
+        System.out.println(solution.getFirstIllegalCharacter());
     }
 }
--- main/java/alperenozil/polestar/test/Solution.java
+++ main/java/alperenozil/polestar/test/Solution.java
@@ -1,9 +1,54 @@
 package alperenozil.polestar.test;
 
-public class Solution {
-    private String name="al";
+import java.util.HashSet;
+import java.util.Stack;
 
-    public String getName() {
-        return name;
+public class Solution {
+    private String input="<([]){()}[{}])";
+    private String findFirstIllegalCharacter(String input) {
+        Stack<Character> characterStack = new Stack();
+        for(int i=0; i<input.length(); i++){
+            char currentCharacter = input.charAt(i);
+            HashSet<Character> openingCharacterSet=new HashSet();
+            openingCharacterSet.add('{');
+            openingCharacterSet.add('[');
+            openingCharacterSet.add('(');
+            openingCharacterSet.add('<');
+            if(openingCharacterSet.contains(currentCharacter)) {
+                characterStack.push(currentCharacter);
+            }
+            else if(currentCharacter == '}') {
+                if(characterStack.size() == 0 || characterStack.peek() != '{') {
+                    return "}";
+                } else {
+                    characterStack.pop();
+                }
+            } else if(currentCharacter == ')') {
+                if(characterStack.size() == 0 || characterStack.peek() != '(') {
+                    return ")";
+                } else {
+                    characterStack.pop();
+                }
+            } else if(currentCharacter == ']') {
+                if(characterStack.size() == 0 || characterStack.peek() != '[') {
+                    return "]";
+                } else {
+                    characterStack.pop();
+                }
+            } else if(currentCharacter == '>') {
+                if(characterStack.size() == 0 || characterStack.peek() != '<') {
+                    return ">";
+                } else {
+                    characterStack.pop();
+                }
+            }
+        }
+        if(characterStack.size() > 0) {
+            return "incomplete";
+        }
+        return "complete";
+    }
+    public String getFirstIllegalCharacter() {
+        return findFirstIllegalCharacter(input);
     }
 }
